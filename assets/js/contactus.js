bcb05f435f3ccaeef6299d5bc7605a3475aab635
AsmaaAamir <asma.liaqat@outlook.com>
2023-02-28 23:42:23 +0000

    added email js to contact us form and contatc js file

---
+++ assets/js/contactus.js
@@ -0,0 +1,13 @@
+// contact us EmailJS from -https://www.emailjs.com/docs/tutorial/creating-contact-form/ 
+window.onload =function(){
+    document.getElementById('contact-form').addEventListener("submit", function (event) {
+        event.preventDefault();
+        emailjs.init('VdTBCASn-iVtZo3xC');
+        emailjs.sendForm('abakes', 'abakes', this)
+            .then (function(){
+                alert('Thank you for your message..Your message has been fowarded to the A Bakes team and they get back to you.")  ');
+            }, function(error) {
+                console.log('Failed....', error);
+            });
+    });
+}
