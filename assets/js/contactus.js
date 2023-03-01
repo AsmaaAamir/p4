e35a56031b55227a0245f0f850bd5160b01e493e
AsmaaAamir <asma.liaqat@outlook.com>
2023-03-01 00:24:45 +0000

    =js email

--- assets/js/contactus.js
+++ assets/js/contactus.js
@@ -1,13 +1,16 @@
 // contact us EmailJS from -https://www.emailjs.com/docs/tutorial/creating-contact-form/ 
 window.onload =function(){
-    document.getElementsByClassName('contact-form').addEventListener("submit", function (event) {
-        event.preventDefault();
+    document.getElementsById('contact').addEventListener('submit', function(event) {
+        event.preventDefault(); 
+        this.contact_number.value = Math.random() * 100000 | 0;
         emailjs.init('VdTBCASn-iVtZo3xC');
-        emailjs.sendForm('abakes', 'abakes', this)
+        emailjs.sendForm('abakes', 'abakes-template', this)
             .then (function(){
-                alert('Thank you for your message..Your message has been fowarded to the A Bakes team and they get back to you.")  ');
+                console.log('SUCCESS!');
             }, function(error) {
                 console.log('Failed....', error);
             });
     });
 }
+
+
