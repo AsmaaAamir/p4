ad302e31cbc0c134bdbb5b99af6cb615f0b4edbc
AsmaaAamir <asma.liaqat@outlook.com>
2023-03-01 00:17:18 +0000

    contact us fomr

--- assets/js/contactus.js
+++ assets/js/contactus.js
@@ -1,6 +1,6 @@
 // contact us EmailJS from -https://www.emailjs.com/docs/tutorial/creating-contact-form/ 
 window.onload =function(){
-    document.getElementById('contact-form').addEventListener("submit", function (event) {
+    document.getElementsByClassName('contact-form').addEventListener("submit", function (event) {
         event.preventDefault();
         emailjs.init('VdTBCASn-iVtZo3xC');
         emailjs.sendForm('abakes', 'abakes', this)
