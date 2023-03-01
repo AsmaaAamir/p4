0f0106f6093656603a3faea6bacf8fe8c87b96a9
AsmaaAamir <asma.liaqat@outlook.com>
2023-03-01 11:19:47 +0000

    change the id to class

--- assets/js/contactus.js
+++ assets/js/contactus.js
@@ -1,9 +1,8 @@
     window.onload = function() {
-        document.getElementById('contact-form').addEventListener('submit', function(event) {
+        document.getElementById('contact').addEventListener("submit", function(event){
             event.preventDefault();
             // generate a five digit number for the contact_number variable
             this.contact_number.value = Math.random() * 100000 | 0;
-            // these IDs from the previous steps
             emailjs.sendForm('abakes', 'abakes-template', this)
                 .then(function() {
                     console.log('SUCCESS!');
