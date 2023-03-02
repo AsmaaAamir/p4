618c3cda1dfad934b1238f80853a64514be64cd1
AsmaaAamir <asma.liaqat@outlook.com>
2023-03-02 18:32:41 +0000

    adding forgotten password to login page

--- assets/js/contactus.js
+++ assets/js/contactus.js
@@ -2,7 +2,6 @@
         document.getElementById('contact').addEventListener("submit", function(event){
             event.preventDefault();
             // generate a five digit number for the contact_number variable
-            this.contact_number.value = Math.random() * 100000 | 0;
             emailjs.sendForm('abakes', 'abakes-template', this)
                 .then(function() {
                     console.log('SUCCESS!');
